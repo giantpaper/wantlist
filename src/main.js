43f6b1a13eeb91b7a265774392ac9b60f2c14549
giantpaper <giantpaper@outlook.com>
2022-12-15 06:21:49 +0000

    Added Storyblok and components

--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
 import { createApp } from 'vue'
+import { StoryblokVue, apiPlugin } from '@storyblok/vue'
 import App from './App.vue'
+// Components
+import Item from './components/Item.vue'
+import Heading from './components/Heading.vue'
+import Grid from './components/Grid.vue';
+import Page from './components/Page.vue';
+import Teaser from './components/Teaser.vue';
+import Feature from './components/Feature.vue';
+
 import './assets/main.scss'
 
+const app = createApp(App)
 
-createApp(App).mount('#app')
+app.use(StoryblokVue, {
+	accessToken: import.meta.env.VITE_PREVIEW_TOKEN,
+	use: [apiPlugin],
+})
+app.component('Item', Item)
+app.component('Heading', Heading)
+app.component('Grid', Grid);
+app.component('Page', Page);
+app.component('Teaser', Teaser);
+app.component('Feature', Feature);
+
+app.mount('#app')
