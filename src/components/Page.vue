c3199604869ba5d207b126fc1b73686d39f1f7e8
giantpaper <giantpaper@outlook.com>
2022-12-15 06:22:25 +0000

    Placeholder components/icons

---
+++ src/components/Page.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-editable="blok" class="px-6">
+    <StoryblokComponent
+      v-for="blok in blok.body"
+      :blok="blok"
+      :key="blok._uid"
+    />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  blok: {
+    type: Object,
+    required: true,
+  }
+});
+</script>
