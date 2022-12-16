3328cc20eeb13f881b1f2dc1f2cdd135b2798d01
giantpaper <giantpaper@outlook.com>
2022-12-16 07:21:15 +0000

    Added support for image copyright notice

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,10 +1,14 @@
 <template>
 	<section v-editable="blok" class="blok item">
-		<img v-if="blok.Image" :src="blok.Image.filename + '/m/230x0/'" :alt="blok.Image.alt" class="mx-auto"
-			:srcset="blok.Image.filename + '/m/460x0/ 2x'"
-			width="230"
-			height="130"
-		/>
+		<figure v-if="blok.Image.filename!==''">
+			<img :src="blok.Image.filename + '/m/230x0/'" :alt="blok.Image.alt" class="mx-auto"
+				:srcset="blok.Image.filename + '/m/460x0/ 2x'"
+				width="230"
+				height="130"
+			/>
+			<figcaption class="text-xs" v-if="blok.Image.copyright">{{ blok.Image.copyright }}</figcaption>
+		</figure>
+		<div v-if="blok.Image.filename===''" class="img_blank" />
 		<div v-for="p in blok.Description.content" class="flex flex-col justify-stretch">
 			<h3 class="text-lg">{{ blok.Name }}</h3>
 			<p v-for="q in p.content">{{ q.text }}</p>
@@ -30,6 +34,7 @@
 			font: 700 1.5rem brevia,sans-serif;
 			margin-bottom: 0.125rem;
 		}
+		.img_blank,
 		img {
 			display: block;
 			max-width: 100%;
@@ -38,6 +43,10 @@
 			@media (max-width: 600px) {
 				max-width: 230px;
 			}
+		}
+		.img_blank {
+			background: currentcolor;
+			aspect-ratio: 230/130;
 		}
 		.button {
 			font-family: brevia, sans-serif;
@@ -61,6 +70,18 @@
 				left: 0;
 				right: 0;
 				bottom: 0;
+		}
+	}
+	figure {
+		position: relative;
+		figcaption {
+			background: transparentize(white, 0.5);
+			position: absolute;
+			bottom: 0;
+			right: 0;
+			padding: 0.25rem;
+			border-radius: 0.25rem 0 0 0;
+			color: var(--vt-c-black);
 		}
 	}
 	i {
